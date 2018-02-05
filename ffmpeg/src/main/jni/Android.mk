f174ff50e1e45db51183b3301df6ee41b41b8ff7
李明艺 <lmyooyo@gmail.com>
2018-02-05 15:04:18 +0800

    更新ffmpeg库

--- ffmpeg/src/main/jni/Android.mk
+++ ffmpeg/src/main/jni/Android.mk
@@ -17,11 +17,6 @@ LOCAL_MODULE := avcodec
 LOCAL_SRC_FILES := libavcodec-56.so
 include $(PREBUILT_SHARED_LIBRARY)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := avdevice
-#LOCAL_SRC_FILES := libavdevice-56.so
-#include $(PREBUILT_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := avfilter
 LOCAL_SRC_FILES := libavfilter-5.so
@@ -53,11 +48,22 @@ LOCAL_SRC_FILES := libswscale-3.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := avresample
+LOCAL_SRC_FILES := libavresample-2.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := postproc
+LOCAL_SRC_FILES := libpostproc-53.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_LDFLAGS += -fPIC
 LOCAL_LDLIBS    := -lm -llog
 # -g 后面的一系列附加项目添加了才能使用 arm_neon.h 头文件
 LOCAL_CFLAGS += -std=c99 -g -mfloat-abi=softfp -mfpu=neon -march=armv7-a -mtune=cortex-a8
 LOCAL_MODULE := ffmpeg
-LOCAL_SRC_FILES =: com_lmy_ffmpeg_Version.c com_lmy_ffmpeg_player_Player.c com_lmy_ffmpeg_codec_MediaDecoder.c com_lmy_ffmpeg_codec_AudioDecoder.c
+LOCAL_SRC_FILES =: com_lmy_ffmpeg_Version.c com_lmy_ffmpeg_player_Player.c com_lmy_ffmpeg_codec_MediaDecoder.c com_lmy_ffmpeg_codec_AudioDecoder.c com_lmy_ffmpeg_codec_VideoEncoder.c
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
 # 采用NEON优化技术
 LOCAL_ARM_NEON := true
@@ -65,5 +71,5 @@ endif
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -llog -lz -ljnigraphics -landroid -lm -pthread
-LOCAL_SHARED_LIBRARIES := avcodec avdevice avfilter avformat avutil swresample swscale libyuv
+LOCAL_SHARED_LIBRARIES := avcodec avfilter avformat avutil swresample swscale avresample postproc libyuv
 include $(BUILD_SHARED_LIBRARY)
